dd066867e838e089621c031bd074d4bce18a34f5
Bruno Reis <58519484+brunorvn@users.noreply.github.com>
2023-05-23 16:41:17 -0300

    Update main.py

--- 2023-05-22/main.py
+++ 2023-05-22/main.py
@@ -1,9 +1,23 @@
 from pyniryo2 import *
 
-robot = NiryoRobot("10.10.10.10")
+robot = NiryoRobot("169.254.200.200")
 
-robot.calibrate_auto()
+robot.arm.calibrate_auto()
 
-robot.move_joints(0.2, -0.3, 0.1, 0.0, 0.5, -0.8)
+# robot.move_joints(0.2, -0.3, 0.1, 0.0, 0.5, -0.8)
 
-robot.close_connection() 
+robot.arm.move_joints([-1.356, -0.237, -0.899, 0.169, -0.063, 0.034]) #safe
+
+robot.tool.release_with_tool()
+
+robot.arm.move_joints([ -1.339, -0.426, -0.797, 0.192, -0.066, -0.007]) # pick
+
+robot.tool.grasp_with_tool()
+
+robot.arm.move_joints([-1.356, -0.237, -0.899, 0.169, -0.063, 0.034]) #safe
+
+robot.arm.move_joints([-0.311, -0.545, -0.769, 0.198, -0.196, -0.549])
+
+robot.tool.release_with_tool()
+
+# robot.close_connection() 
